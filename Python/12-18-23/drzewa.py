aa65a9039867474eb673499bc8d60a5a8ec62d49
Bartek <bmroczkowski@icloud.com>
2024-01-10 21:48:38 +0100

    Python 10/01/2024

--- Python/12-18-23/drzewa.py
+++ Python/12-18-23/drzewa.py
@@ -1,99 +1,66 @@
 from random import randint
 
-# Tasks 1-5
-print("# Tasks 1-5")
 
-
+# 1-5
 class Drzewko:
-    """Klasa reprezentująca drzewo"""
+    """Reprezentacja Drzewka"""
 
     rodzina = "drzewa"
     typ_pnia = "drewno"
-    index: int
 
-    def __init__(self, gatunek, wiek, wysokosc):
+    def __init__(self, gatunek, wiek, wysokosc, index=0):
         self.gatunek = gatunek
         self.wiek = wiek
         self.wysokosc = wysokosc
+        self.index = index
 
-        print("Zasadzono nowe drzewo o gatunku", gatunek)
+        print("Zasadzono nowe drzewo o gatunku:", gatunek)
 
-    def info_in(self):
-        print(
-            f"""Drzewo:
-    rodzina: {self.rodzina}
-    typ pnia: {self.typ_pnia}
-    gatunek: {self.gatunek}
-    wysokość: {self.wysokosc} cm
-    wiek: {self.wiek} {'rok' if self.wiek == 1 else 'lat'}
-        """
-        )
+    def info_IN(self):
+        print(f"Drzewo - typ pnia: {self.typ_pnia}, gatunek: {self.gatunek}, wysokość: {self.wysokosc} cm, wiek: {self.wiek} rok/lat")
 
-    def rosne_in(self):
+    def rosne_IN(self):
         self.wysokosc += randint(50, 150)
         self.wiek += 1
 
     def __del__(self):
-        print(f"Drzewo o indeksie: {self.index}, gatunku: {self.gatunek}, wieku: {self.wiek}, wysokości: {self.wysokosc}, zostało ścięte")
+        print(f"Ścięto drzewo - index: {self.index}, gatunek: {self.gatunek}, wiek: {self.wiek}, wysokość: {self.wysokosc}")
 
 
-print("Utworzono klasę Drzewko")
+# 6
+with open("./drzewa.txt", "r", encoding="UTF-8") as f:
+    las_IN = [
+        Drzewko(gatunek.strip(), randint(1, 5), randint(20, 150), index=i)
+        for i, gatunek in enumerate(f.readlines())  # [(0, gatunek 1), (1, gatunek 2)]
+    ]
 
 
-# Funkcja do wyświetlania lasu
+# 7
 def wyswietl_las(las):
-    print("Wyświetlanie lasu:")
+    print("\nLAS:")
 
     for drzewo in las:
-        drzewo.info_in()
+        drzewo.info_IN()
+
+    print("")
 
 
-# Task 6
-print("\n# Task 6")
-las_in = []
+wyswietl_las(las_IN)
 
-with open("./drzewa.txt", "r", encoding="UTF-8") as f:
-    gatunki = [line.strip() for line in f.readlines()]
+# 8
+print("Właściwości klasy:", [element for element in dir(Drzewko) if not element.startswith("__")])
+print("Wartości obiektu:", las_IN[0].__dict__, "\n")
 
-for ind, gatunek in enumerate(gatunki):
-    drzewo = Drzewko(gatunek, randint(1, 5), randint(20, 150))
-    drzewo.index = ind
+# 9
+for drzewo in las_IN:
+    drzewo.rosne_IN()
+    drzewo.rosne_IN()
 
-    las_in.append(drzewo)
+# 10
+del las_IN[-1]
 
-print("Utworzono las")
+# 11
+las_IN.append(Drzewko("bonzai", randint(1, 5), randint(20, 150), index=len(las_IN)))
 
-# Task 7
-print("\n# Task 7")
-wyswietl_las(las_in)
-
-# Task 8
-print("\n# Task 8")
-print("Właściwości klasy Drzewko:", [item for item in dir(Drzewko) if not item.startswith("__")])
-print("\nWłaściwości jednego z drzew:", [item for item in dir(las_in[0]) if not item.startswith("__")])
-print("Własności jednego z drzew:", las_in[0].__dict__)
-
-# Task 9
-print("\n# Task 9")
-
-for drzewo in las_in:
-    drzewo.rosne_in()
-    drzewo.rosne_in()
-
-print("Minęły 2 lata")
-
-# Task 10
-print("\n# Task 10")
-del las_in[-1]
-print("Usunięto ostatnie drzewo")
-
-# Task 11
-print("\n# Task 11")
-drzewo = Drzewko(gatunki[-1], randint(1, 5), randint(20, 150))
-drzewo.index = len(las_in)
-
-las_in.append(drzewo)
-
-# Task 12
-print("\n# Task 12")
-wyswietl_las(las_in)
+# 12
+wyswietl_las(las_IN)
